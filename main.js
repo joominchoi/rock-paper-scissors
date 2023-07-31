58d7ab4462bc25eb090c2802f5acb0d9fff444ef
JooMin Choi <joominchoi23@gmail.com>
2023-07-31 17:11:03 +0100

    Create playRound function
    - Compares playerSelection and computerSelection and returns a starting
    depending on win, loss, draw criteria

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 function getComputerChoice() {
-	let choices = ["Rock", "Paper", "Scissors"]
-	
+	let choices = ["Rock", "Paper", "Scissors"];
+
 	let randomChoice = choices[Math.floor(Math.random() * choices.length)];
 
-	console.log(randomChoice);
+	return randomChoice
+
 }
 
-getComputerChoice()
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === "Rock" && computerSelection === "Scissors") {
+		return "You Win! Rock beats Scissors";
+	} else if (playerSelection === "Paper" && computerSelection === "Rock") {
+		return "You Win! Paper beats Rock";
+	} else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+		return "You Win! Scissors beats Paper";
+	} else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+		return "You Lose! Rock beats Scissors";
+	} else if (computerSelection === "Paper" && playerSelection === "Rock") {
+		return "You Lose! Paper beats Rock";
+	} else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+		return "You Lose! Scissors beats Paper";
+	} else if (playerSelection === "Rock" && computerSelection === "Rock") {
+		return "Its a Draw! You both drew Rock!"
+	} else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+		return "Its a Draw! You both drew Scissors!"
+	} else if (playerSelection === "Rock" && computerSelection === "Rock") {
+		return "Its a Draw! You both drew Rock!"
+	} else return "Please check input"
+}
+
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
